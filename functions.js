a1400e334163230a9e24c8d07c688122f57bd3dc
Ihtishaam Johnson <ihtishaamj63@gmail.com>
2026-05-29 11:31:39 +0200

    correction

--- functions.js
+++ functions.js
@@ -1,26 +1,20 @@
-// TODO: Create a function called ‘multiply’ that will take in 3 parameter andmultiplyall 3 numbers together. Note: The argument passed must NOT be the same number. // TODO: Create a function expression called ‘convertToSeconds’ that takes inanumber of minutes as an argument and returns the amount in seconds. ***Donotuse a console.log() inside of the function***
-// TODO: Create a function called fahrenheitToCelsius that takes the temperatureasan parameter and returns the equivalent temperature in Celsius. ***Do not useaconsole.log() inside of the function***
-// TODO: Create a function that takes a string as a parameter and returns thereverseof the string. ***Do not use a console.log() inside of the function ***
-// TODO: Create a function that takes in a string and returns the number of Vowelsinthe sentence. E.g. countVowels(‘Javascript’) //output =3 ***Do not useaconsole.log() inside of the function***
-// TODO: Create a “isPrime” function that takes a number as a parameter andreturnstrue if the number is prime, and false otherwise. Make sure to test your codewith4numbers
-
 function multiply(x, y, z) {
   return x * y * z;
 }
 
 console.log(multiply(5, 10, 3));
 
-function convertToSeconds(seconds) {
-  return seconds * 60;
-}
+const convertToSeconds = function(minutes) {
+  return minutes * 60;
+};
 
 console.log(convertToSeconds(1));
 
-function fahrneheitToCelsius(fahrenheit) {
+function fahrenheitToCelsius(fahrenheit) {
   return ((fahrenheit - 32) * 5) / 9;
 }
 
-console.log(fahrneheitToCelsius(40));
+console.log(fahrenheitToCelsius(40));
 
 function reverseString(str) {
   return str.split("").reverse().join("");
@@ -29,7 +23,7 @@ function reverseString(str) {
 console.log(reverseString("76"));
 
 function countVowels(str) {
-  const vowels = "aeiouAIEOU";
+  const vowels = "aeiouAEIOU";
   let count = 0;
   for (let i = 0; i < str.length; i++) {
     if (vowels.includes(str[i])) {
@@ -42,6 +36,7 @@ function countVowels(str) {
 console.log(countVowels("Hello guys"));
 
 function isPrime(number) {
+  if (number <= 1) return false;
   for (let i = 2; i < number; i++) {
     if (number % i === 0) {
       return false;
@@ -51,3 +46,6 @@ function isPrime(number) {
 }
 
 console.log(isPrime(5));
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(15));
